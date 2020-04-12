95110ff27f4ad93077ea69b724c1167ca56bca93
actionbronson <plegault@gmail.com>
2020-04-12 09:40:14 -0400

    Renamed to Weekly.

---
+++ weekly-api-server/src/gen/java/org/weekly/model/User.java
@@ -0,0 +1,106 @@
+package org.weekly.model;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+import io.swagger.annotations.ApiModelProperty;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User  {
+  
+  @ApiModelProperty(required = true, value = "")
+  private Long id = null;
+
+  @ApiModelProperty(required = true, value = "")
+  private String name = null;
+
+  @ApiModelProperty(value = "")
+  private String alias = null;
+ /**
+   * Get id
+   * @return id
+  **/
+  @JsonProperty("id")
+  @NotNull
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public User id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+ /**
+   * Get name
+   * @return name
+  **/
+  @JsonProperty("name")
+  @NotNull
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public User name(String name) {
+    this.name = name;
+    return this;
+  }
+
+ /**
+   * Get alias
+   * @return alias
+  **/
+  @JsonProperty("alias")
+  public String getAlias() {
+    return alias;
+  }
+
+  public void setAlias(String alias) {
+    this.alias = alias;
+  }
+
+  public User alias(String alias) {
+    this.alias = alias;
+    return this;
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class User {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    alias: ").append(toIndentedString(alias)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private static String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
